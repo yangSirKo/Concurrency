bd2df1019c2e86fcff61ecba409e7e39537ddf35
yangjinpeng <1415946318@qq.com>
2018-05-04 12:52:09 +0800

    init

---
+++ src/main/java/com/atyang/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.atyang.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ConcurrencyApplication.class, args);
+    }
+
+}
